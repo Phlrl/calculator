d5c3370c5abe65aba6d8d97f9b94b7e0bafdcd39
Phlrl <philipp129999@gmail.com>
2026-09-03 16:45:48 +0200

    first implementation

---
+++ calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace calculator
+{
+    internal class Taschenrechner
+    {
+
+        static void Main(string[] args)
+        {
+            AddierenEingaben();
+        }
+
+        public static void AddierenEingaben()
+        {
+            Console.WriteLine("Summand 1 :");
+            string eingabe = Console.ReadLine();
+            int summannd1 = Int32.Parse(eingabe);
+
+            Console.WriteLine("Summand 2 :");
+            eingabe = Console.ReadLine();
+            int summannd2 = Int32.Parse(eingabe);
+            int summe = Addieren(summannd1, summannd2);
+
+            Console.WriteLine(summe);
+            Thread.Sleep(10000);
+        }
+
+        public static int Addieren(int Summand1, int Summand2)
+        {
+            int Summe = Summand1 + Summand2;
+            return Summe;
+        }
+    }
+}
